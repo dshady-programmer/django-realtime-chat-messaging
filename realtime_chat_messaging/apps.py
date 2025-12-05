da64d3a9291c0bcfd853b4a4dd257d7f71f8371f
shady-cj <petersp2000@gmail.com>
2025-12-05 15:02:13 +0100

    develop: signals to delete channels with no subscribers

--- realtime_chat_messaging/apps.py
+++ realtime_chat_messaging/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class RealtimeChatMessagingConfig(AppConfig):
     name = 'realtime_chat_messaging'
+
+    def ready(self):
+        from . import signals
+        return super().ready()
--- realtime_chat_messaging/signals.py
+++ realtime_chat_messaging/signals.py
@@ -16,4 +16,21 @@ def enforce_two_participants_on_one_to_one_chat(sender, instance, action, pk_set
 @receiver(post_save, sender=Channel)
 def add_creator_as_participant_and_admin(sender, instance, created, *args, **kwargs):
     if created:
-        instance.creator
+        if sender == GroupChat:
+            instance.participants.add(instance.creator)
+            instance.admins.add(instance.creator)
+        else:
+            instance.subscribers.add(instance.creator)
+            instance.moderators.add(instance.creator)
+
+
+@receiver(m2m_changed, sender=GroupChat.participants.through)
+@receiver(m2m_changed, sender=Channel.subscribers.through)
+def delete_channels_and_groups_with_no_participants(sender, instance, action, pk_set, **kwargs):
+    if action == "post_remove" or action == "post_clear":
+        if (hasattr(instance, "participants") and instance.participants.count() < 1) or (hasattr(instance, "subscribers") and instance.subscribers.count() < 1):
+            instance.delete()
+
+
+
+    
